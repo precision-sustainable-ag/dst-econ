e52f492ef3005a8f4d0bbc1c577640ede06ceb61
Rick Hitchcock <rickhitchcock@outlook.com>
2023-03-17 18:50:43 -0400

    lint Termination

--- src/components/Termination/index.js
+++ src/components/Termination/index.js
@@ -1,5 +1,5 @@
-/* eslint-disable */
-import {useSelector, useDispatch} from 'react-redux';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import {
   get,
@@ -16,78 +16,89 @@ import {
 } from '../../store/Store';
 
 import Logic from '../Logic';
-import {ClearInputs} from '../ClearInputs';
-import {Input} from '../../shared/Inputs';
+import { ClearInputs } from '../ClearInputs';
+import { Input } from '../../shared/Inputs';
 
-const defaults = getDefaults('termination.additionalHerbicides|termination.additionalPrices|termination.additionalRates|termination.reducedHerbicides|termination.reducedPrices|termination.reducedRates|termination.q2|chemical.implement|chemical.power|chemical.implementsCost|chemical.powerCost|chemical.Labor|chemical.Fuel|chemical.Depreciation|chemical.Interest|chemical.Repairs|chemical.Taxes|chemical.Insurance|chemical.Storage|roller.implement|roller.power|roller.implementsCost|roller.powerCost|roller.Labor|roller.Fuel|roller.Depreciation|roller.Interest|roller.Repairs|roller.Taxes|roller.Insurance|roller.Storage|tillage.implement|tillage.power|tillage.implementsCost|tillage.powerCost|tillage.Labor|tillage.Fuel|tillage.Depreciation|tillage.Interest|tillage.Repairs|tillage.Taxes|tillage.Insurance|tillage.Storage|termination.method|termination.customCost|termination.product');
+const defaults = getDefaults([
+  'termination.additionalHerbicides', 'termination.additionalPrices', 'termination.additionalRates', 'termination.reducedHerbicides',
+  'termination.reducedPrices', 'termination.reducedRates', 'termination.q2',
+  'chemical.implement', 'chemical.power', 'chemical.implementsCost', 'chemical.powerCost', 'chemical.Labor', 'chemical.Fuel',
+  'chemical.Depreciation', 'chemical.Interest', 'chemical.Repairs', 'chemical.Taxes', 'chemical.Insurance', 'chemical.Storage',
+  'roller.implement', 'roller.power', 'roller.implementsCost', 'roller.powerCost', 'roller.Labor', 'roller.Fuel',
+  'roller.Depreciation', 'roller.Interest', 'roller.Repairs', 'roller.Taxes', 'roller.Insurance', 'roller.Storage',
+  'tillage.implement', 'tillage.power', 'tillage.implementsCost', 'tillage.powerCost', 'tillage.Labor', 'tillage.Fuel',
+  'tillage.Depreciation', 'tillage.Interest', 'tillage.Repairs', 'tillage.Taxes', 'tillage.Insurance', 'tillage.Storage',
+  'termination.method', 'termination.customCost', 'termination.product',
+]);
 
-const HerbicidesRow = ({n, prop}) => {
+const HerbicidesRow = ({ n, prop }) => {
   const additional = useSelector(get.termination.additionalHerbicides);
-  const reduced    = useSelector(get.termination.reducedHerbicides);
+  const reduced = useSelector(get.termination.reducedHerbicides);
   const used = [...additional, ...reduced];
   return (
     <tr>
       <td>
         <Input
-          id={'termination.' + prop + 'Herbicides'}
+          id={`termination.${prop}Herbicides`}
           index={n}
-          options={Object.keys(db.herbicides).filter(s => !used.includes(s)).sort()}
+          options={Object.keys(db.herbicides).filter((s) => !used.includes(s)).sort()}
         />
       </td>
       <td>
         <Input
           type="number"
-          id={'termination.' + prop + 'Rates'}
+          id={`termination.${prop}Rates`}
           index={n}
         />
       </td>
       <td>
         <Input
           type="dollar"
-          id={'termination.' + prop + 'Prices'}
+          id={`termination.${prop}Prices`}
           index={n}
         />
       </td>
     </tr>
-  )
-} // HerbicidesRow
+  );
+}; // HerbicidesRow
 
-const OtherHerbicides = ({state, prop, description}) => {
-  return (
-    <table style={{width: '100%'}}>
-      <caption>{description}</caption>
-      <thead>
-        <tr>
-          <th>Herbicide</th>
-          <th>Rate (lb/acre)</th>
-          <th>Price ($/acre)</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          state[prop + 'Herbicides'].map((s, n) => {
-            return (
-              s &&
-              <HerbicidesRow key={n} n={n} prop={prop} />
-            )
-          })
+const OtherHerbicides = ({ state, prop, description }) => (
+  <table style={{ width: '100%' }}>
+    <caption>{description}</caption>
+    <thead>
+      <tr>
+        <th>Herbicide</th>
+        <th>Rate (lb/acre)</th>
+        <th>Price ($/acre)</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+          state[`${prop}Herbicides`].map((s, n) => (
+            s
+              && <HerbicidesRow key={n} n={n} prop={prop} />
+          ))
         }
-        <HerbicidesRow key={state[prop + 'Herbicides'].length} n={state[prop + 'Herbicides'].length} prop={prop} />
-      </tbody>
-      <tfoot>
-        <tr>
-          <td colSpan="3" style={{textAlign: 'right', background: 'yellow'}}>
-            Total {prop === 'additional' ? 'Costs' : 'Benefits'}: {dollars(state[prop + 'Total'])}
-          </td>
-        </tr>
-      </tfoot>
-    </table>
-  )
-} // OtherHerbicides
+      <HerbicidesRow key={state[`${prop}Herbicides`].length} n={state[`${prop}Herbicides`].length} prop={prop} />
+    </tbody>
+    <tfoot>
+      <tr>
+        <td colSpan="3" style={{ textAlign: 'right', background: 'yellow' }}>
+          Total
+          {' '}
+          {prop === 'additional' ? 'Costs' : 'Benefits'}
+          :
+          {' '}
+          {dollars(state[`${prop}Total`])}
+        </td>
+      </tr>
+    </tfoot>
+  </table>
+); // OtherHerbicides
 
 const Herbicide = () => {
   const state = useSelector(get.termination);
-  
+
   return (
     <>
       <tr><th colSpan="100">Product</th></tr>
@@ -102,7 +113,7 @@ const Herbicide = () => {
         current="termination"
         property="unitCost"
         q="Cost per unit of product"
-        a={'dollar'}
+        a="dollar"
         suffix={db.herbicides[state.product]?.['Unit (cost)']}
       />
 
@@ -110,7 +121,7 @@ const Herbicide = () => {
         current="termination"
         property="rate"
         q="Application rate"
-        a={'number'}
+        a="number"
         suffix={db.herbicides[state.product]?.['Unit (rate)']}
       />
 
@@ -139,10 +150,10 @@ const Herbicide = () => {
         current="chemical"
         question="Estimated"
         q="Chemical spray equipment cost ($/acre)"
-        a={'dollar'}
+        a="dollar"
       />
     </>
-  )
+  );
 }; // Herbicide
 
 const Roller = () => (
@@ -165,7 +176,7 @@ const Roller = () => (
       current="roller"
       question="Estimated"
       q="Roller equipment cost ($/acre)"
-      a={'dollar'}
+      a="dollar"
     />
   </>
 ); // Roller
@@ -190,13 +201,13 @@ const Tillage = () => (
       current="tillage"
       question="Estimated"
       q="Tillage equipment cost ($/acre)"
-      a={'dollar'}
+      a="dollar"
     />
   </>
 ); // Tillage
 
 const Termination = () => {
-  console.log('Render: Termination');
+  // console.log('Render: Termination');
   const dispatch = useDispatch();
   const current = 'termination';
   const dev = useSelector(get.dev);
@@ -208,18 +219,22 @@ const Termination = () => {
       <div className="Termination">
         <h1>Termination</h1>
         <p>
-          In order to accurately evaluate the economic impact of implementing cover crops into your rotation, we only want to consider management decisions directly associated with the use of cover crops.
-          In this module we will consider any activity associated with termination of the cover crop, but only activities that are incurred specific to the cover crop.
-          For example, if you plan to use tillage to terminate the cover crop but would normally conduct the same tillage pass if no cover crop had been planted then the costs associated with tillage will not be considered in this evaluation.
+          In order to accurately evaluate the economic impact of implementing cover crops into your rotation,
+          we only want to consider management decisions directly associated with the use of cover crops.
+          In this module we will consider any activity associated with termination of the cover crop,
+          but only activities that are incurred specific to the cover crop.
+          For example, if you plan to use tillage to terminate the cover crop but would normally conduct the same tillage pass
+          if no cover crop had been planted then the costs associated with tillage will not be considered in this evaluation.
           This module has four methods of termination; herbicide, tillage, using a crop roller, and using a crop roller combined with herbicide.
         </p>
         <p>
           Also, the costs associated with machinery are dependent upon the annual hours of use.
-          In this module you can accept the default value (for hours of annual use) or customize with the estimated hours associated with your operation.
+          In this module you can accept the default value (for hours of annual use)
+          or customize with the estimated hours associated with your operation.
           This will more accurately represent costs in your operation.
         </p>
         <form>
-          <table className={current + ' inputs'}>
+          <table className={`${current} inputs`}>
             <thead>
               <tr>
                 <th colSpan="2">
@@ -260,7 +275,8 @@ const Termination = () => {
                   intro="
                     Even if you would normally spray a burn-down application, you may elect to add/remove an herbicide or change the rate applied.
                     For example, if clover is part of your cover crop mix you may wish to include 2, 4-D to help sure adequate termination.
-                    Alternatively, some growers have found that use of cover crops (e.g. cereal rye which has an allelopathic effect) allows them to reduce herbicides used in their tank mix.
+                    Alternatively, some growers have found that use of cover crops (e.g. cereal rye which has an allelopathic effect)
+                    allows them to reduce herbicides used in their tank mix.
                   "
                   property="q3"
                   q="Will you change the herbicides use in your tank mix?"
@@ -279,7 +295,7 @@ const Termination = () => {
                   <tr>
                     <td colSpan="3">
                       <OtherHerbicides state={state} description="Additional Herbicides" prop="additional" />
-                      <OtherHerbicides state={state} description="Reduced Herbicides"    prop="reduced" />
+                      <OtherHerbicides state={state} description="Reduced Herbicides" prop="reduced" />
                     </td>
                   </tr>
                 )
@@ -301,18 +317,23 @@ const Termination = () => {
       {
         dev && (
           <>
-            <button onClick={exampleTermination1}>Test Herbicide 1</button>
-            <button onClick={exampleTermination2}>Test Herbicide 2</button>
-            <button onClick={exampleTermination3}>Test Roller</button>
-            <button onClick={exampleTermination4}>Test Tillage</button>
-            <button onClick={exampleTermination5}>Test Roller with follow-up herbicide</button>
+            <button type="button" onClick={exampleTermination1}>Test Herbicide 1</button>
+            <button type="button" onClick={exampleTermination2}>Test Herbicide 2</button>
+            <button type="button" onClick={exampleTermination3}>Test Roller</button>
+            <button type="button" onClick={exampleTermination4}>Test Tillage</button>
+            <button type="button" onClick={exampleTermination5}>Test Roller with follow-up herbicide</button>
           </>
         )
       }
     </>
-  )
-} // Termination
+  );
+}; // Termination
 
-Termination.menu = <span><u>T</u>ermination</span>;
+Termination.menu = (
+  <span>
+    <u>T</u>
+    ermination
+  </span>
+);
 
 export default Termination;
