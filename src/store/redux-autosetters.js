19305431962222699da13d46835d60670fe82fc3
Rick Hitchcock <rickhitchcock@outlook.com>
2023-03-18 14:59:25 -0400

    lint autosetters

--- src/store/redux-autosetters.js
+++ src/store/redux-autosetters.js
@@ -1,138 +1,140 @@
-/* eslint-disable */
-import {configureStore, createAction, createReducer} from '@reduxjs/toolkit';
+/* eslint-disable no-alert */
+/* eslint-disable no-console */
+
+import { configureStore, createAction, createReducer } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const set = {};
 export const get = {};
 
-export const createStore = (initialState, {afterChange={}, reducers={}}) => {
+export const createStore = (initialState, { afterChange = {}, reducers = {} }) => {
   const funcs = {};
   const methods = {};
   const allkeys = {};
 
   const getAllkeys = (obj, parents = []) => {
-    Object.keys(obj).forEach(key => {
+    Object.keys(obj).forEach((key) => {
       const isArray = Array.isArray(obj[key]);
       const isObject = !isArray && obj[key] instanceof Object && typeof obj[key] !== 'function';
-      const fullkey = parents.length ? parents.join('.') + '.' + key : key;
-  
+      const fullkey = parents.length ? `${parents.join('.')}.${key}` : key;
+
       allkeys[fullkey] = true;
-  
+
       if (isObject) {
         getAllkeys(obj[key], [...parents, key]);
-      };
+      }
     });
-  } // getAllkeys
+  }; // getAllkeys
 
   getAllkeys(initialState);
 
   const processMethods = ((state, key) => {
     if (methods[key]) {
-      for (let k in methods[key]) {
+      Object.keys(methods[key]).forEach((k) => {
         let st = state;
-        for (const key of k.split('.').slice(0, -1)) st = st[key];
+        k.split('.').slice(0, -1).forEach((key2) => {
+          st = st[key2];
+        });
         const l = k.includes('.') ? k.split('.').slice(-1)[0] : k;
         st[l] = methods[key][k](state);
         processMethods(state, k);
-      }
+      });
     }
   });
-      
+
   const builders = (builder) => {
-    const recurse = (obj, set, get, parents = []) => {
-      if (!obj) {   // TODO dst-econ
-        console.log(set);
+    const recurse = (obj, set2, get2, parents = []) => {
+      if (!obj) { // TODO dst-econ
+        console.log(set2);
         return;
       }
       Object.keys(obj).forEach((key) => {
         const isArray = Array.isArray(obj[key]);
         const isObject = !isArray && obj[key] instanceof Object;
-        const fullkey = parents.length ? parents.join('.') + '.' + key : key;
+        const fullkey = parents.length ? `${parents.join('.')}.${key}` : key;
         allkeys[fullkey] = true;
-  
-        get[key] = (state) => {
-          let st = state;
-          for (const k of parents) st = st[k];
+
+        get2[key] = (state) => {
+          const st = parents.reduce((acc, k) => acc[k], state);
 
           if (!st) {
-            alert('Unknown: ' + fullkey);
+            alert(`Unknown: ${fullkey}`);
           }
           return st[key];
-        }
+        };
 
         if (typeof obj[key] === 'function') {
           funcs[fullkey] = obj[key];
           const func = obj[key].toString();
 
-          for (const key in allkeys) {
-            if (func.match(new RegExp(`${key.replace(/[.$]/g, c => '\\' + c)}`))) {
-              methods[key] = methods[key] || {};
-              methods[key][fullkey] = funcs[fullkey];
+          Object.keys(allkeys).forEach((key2) => {
+            const regex = new RegExp(`${key2.replace(/[.$]/g, (c) => `\\${c}`)}`);
+            if (func.match(regex)) {
+              methods[key2] = methods[key2] || {};
+              methods[key2][fullkey] = funcs[fullkey];
             }
-          }
-  
+          });
+
           obj[key] = funcs[fullkey](initialState);
         }
-  
-        set[key] = createAction(fullkey);
+
+        set2[key] = createAction(fullkey);
 
         builder
-          .addCase(set[key], (state, action) => {
-            let st = state;
-            for (const k of parents) st = st[k];
+          .addCase(set2[key], (state, action) => {
+            const st = parents.reduce((acc, k) => acc[k], state);
 
             if (isArray && Number.isFinite(action.payload.index)) {
-              const {index, value} = action.payload;
+              const { index, value } = action.payload;
               st[key][index] = value;
             } else {
               st[key] = action.payload;
             }
-            
+
             if (afterChange[fullkey]) {
               const ac = afterChange[fullkey](state, action);
               if (ac) {
-                ac.forEach(parm => afterChange[parm](state, action));
+                ac.forEach((parm) => afterChange[parm](state, action));
               }
             }
 
             // TODO:  Is the first of these needed?
-              processMethods(state, key);
-              processMethods(state, fullkey);
+            processMethods(state, key);
+            processMethods(state, fullkey);
 
             if (afterChange[fullkey]) {
               const func = afterChange[fullkey].toString();
-              for (const key in allkeys) {
-                if (func.match(new RegExp(`${key.replace(/[.$]/g, c => '\\' + c)}`))) {
-                  processMethods(state, key);
+              Object.keys(allkeys).forEach((key2) => {
+                if (func.match(new RegExp(`${key2.replace(/[.$]/g, (c) => `\\${c}`)}`))) {
+                  processMethods(state, key2);
                 }
-              }
+              });
             }
-          }
-        );
-  
+          });
+
         if (isObject) {
-          recurse(obj[key], set[key], get[key], [...parents, key]);
+          recurse(obj[key], set2[key], get2[key], [...parents, key]);
         }
       });
-    } // recurse
-  
-    for (const key in reducers) {
-      const action = createAction(key);
-      builder.addCase(action, reducers[key]);
-    }
+    }; // recurse
 
-    builder.addCase(createAction('api'), (state, {payload}) => {
+    Object.entries(reducers).forEach(([key, reducer]) => {
+      const action = createAction(key);
+      builder.addCase(action, reducer);
+    });
+
+    builder.addCase(createAction('api'), (state, { payload }) => {
       const method = payload.options.method || 'get';
 
       axios[method](payload.url, payload.options)
-        .then(data => {
+        .then((data) => {
           if (typeof payload.callback === 'function') {
             payload.callback(data.data);
           } else {
-            alert('Error: ' + JSON.stringify(payload, null, 2));
+            alert(`Error: ${JSON.stringify(payload, null, 2)}`);
           }
         })
-        .catch(error => {
+        .catch((error) => {
           console.log('api error: ', error);
         });
     });
@@ -141,18 +143,17 @@ export const createStore = (initialState, {afterChange={}, reducers={}}) => {
 
     builder.addDefaultCase((state, action) => {
       if (action.type !== '@@INIT') {
-        console.log('Unknown action: ' + JSON.stringify(action));
+        console.log(`Unknown action: ${JSON.stringify(action)}`);
       }
     });
-  } // builders
+  }; // builders
 
   const reducer = createReducer(initialState, builders);
 
   return configureStore({
     reducer,
-    middleware: getDefaultMiddleware =>
-      getDefaultMiddleware({
-        serializableCheck: false,
-      }),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+      serializableCheck: false,
+    }),
   });
-} // createStore
+}; // createStore
