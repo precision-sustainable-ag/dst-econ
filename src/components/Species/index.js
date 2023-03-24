9216318ea21cb7c923ec32b9c3189673c9ee5957
Rick Hitchcock <rickhitchcock@outlook.com>
2023-03-24 10:54:28 -0400

    Issue #58: Cover Crops

--- src/components/Species/index.js
+++ src/components/Species/index.js
@@ -113,49 +113,10 @@ const Species = () => {
           <h1>Cover Crops Species Selection</h1>
 
           <div id="About">
-            <p>In this section, you will select a cover crop and seeding rates as well as an estimated cost of the cover crop seed ($/pound).</p>
-
             <p>
-              A common strategy for selecting a cover crop is to base your decision upon the need
-              for a specific change you wish to make in a field or farm.
-              For example, if you wish to build soil organic matter
-              you may want to select a cover crop with high levels of biomass and a fibrous root structure.
-              Options exist to mitigate soil erosion, create grazing opportunities, and to decrease weed pressure.
-              If you already know which cover crop you wish to consider, please select from the drop-down menu.
-              If you are new to the use of cover crops or still have questions,
-              please consider using the
-              {' '}
-              <a href={link} target="_blank" rel="noreferrer">
-                {region}
-                {' '}
-                Cover Crops Council Species Selector
-              </a>
-              .
-            </p>
-
-            <p>
-              After selecting a cover crop species, please select a seeding rate.
-              Seeding rate selection will depend upon whether you are using a single species or a mix.
-              In addition, you may adjust seeding rates even for single species based upon your intended purpose.
-              For example, you may use a lower seeding rate of cereal rye if you only seek help with erosion control,
-              but may plant a higher rate if you seek to maximize grazing potential.
-              You may use the common rate listed in the green shaded boxes or input your own rate.
-              If you wish to learn more about possible seeding rates, consider using the &quot;Cover Crop Seeding Rate Calculator&quot;.
-            </p>
-
-            <p>
-              Prices for cover crops may vary widely based upon your geography and availability from season to season.
-              You may utilize the default value or enter specific pricing from your region.
-              Growers planning to plant a cover crop
-              {' '}
-              <em>mix</em>
-              {' '}
-              can either &apos;build&apos; their mix by selecting multiple cover crop species and an
-              {' '}
-              <em><strong>appropriate</strong></em>
-              {' '}
-              seeding rate (i.e. seeding rate may be lower in a mix relative to seeding as a stand-alone)
-              or by selecting commercial mix and entering a seed rate and price for the commercial mix you chose.
+              In the User box below, select your cover crop seed(s).
+              Default seeding rates and prices will appear.
+              You are encouraged to adjust these to fit your farming plan.
             </p>
           </div>
 
@@ -198,6 +159,50 @@ const Species = () => {
               </tr>
             </tbody>
           </table>
+
+          <p>
+            A common strategy for selecting a cover crop is to base your decision upon the need
+            for a specific change you wish to make in a field or farm.
+            For example, if you wish to build soil organic matter
+            you may want to select a cover crop with high levels of biomass and a fibrous root structure.
+            Options exist to mitigate soil erosion, create grazing opportunities, and to decrease weed pressure.
+            If you already know which cover crop you wish to consider, please select from the drop-down menu.
+            If you are new to the use of cover crops or still have questions,
+            please consider using the
+            {' '}
+            <a href={link} target="_blank" rel="noreferrer">
+              {region}
+              {' '}
+              Cover Crops Council Species Selector
+            </a>
+            .
+          </p>
+
+          <p>
+            After selecting a cover crop species, please select a seeding rate.
+            Seeding rate selection will depend upon whether you are using a single species or a mix.
+            In addition, you may adjust seeding rates even for single species based upon your intended purpose.
+            For example, you may use a lower seeding rate of cereal rye if you only seek help with erosion control,
+            but may plant a higher rate if you seek to maximize grazing potential.
+            You may use the common rate listed in the green shaded boxes or input your own rate.
+            If you wish to learn more about possible seeding rates, consider using the &quot;Cover Crop Seeding Rate Calculator&quot;.
+          </p>
+
+          <p>
+            Prices for cover crops may vary widely based upon your geography and availability from season to season.
+            You may utilize the default value or enter specific pricing from your region.
+            Growers planning to plant a cover crop
+            {' '}
+            <em>mix</em>
+            {' '}
+            can either &apos;build&apos; their mix by selecting multiple cover crop species and an
+            {' '}
+            <em><strong>appropriate</strong></em>
+            {' '}
+            seeding rate (i.e. seeding rate may be lower in a mix relative to seeding as a stand-alone)
+            or by selecting commercial mix and entering a seed rate and price for the commercial mix you chose.
+          </p>
+
         </div>
       </form>
 
@@ -210,8 +215,9 @@ const Species = () => {
 
 Species.menu = (
   <span>
-    <u>C</u>
-    over Crops
+    See
+    <u>d</u>
+    s
   </span>
 );
 
