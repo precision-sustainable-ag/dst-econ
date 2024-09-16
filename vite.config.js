44883f0ac87f542275006b180daa3ac50c3c65e3
Rick Hitchcock <rickhitchcock@outlook.com>
2024-09-16 17:55:14 -0400

    test vite k8s

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: true,
-    watch: {
-      usePolling: true,
-    },
-    host: true, // needed for the Docker Container port mapping to work
-    strictPort: true,
-    port: 3000,
-  },
+  // server: {
+  //   open: true,
+  //   watch: {
+  //     usePolling: true,
+  //   },
+  //   host: true, // needed for the Docker Container port mapping to work
+  //   strictPort: true,
+  //   port: 3000,
+  // },
   build: {
     outDir: '/usr/src/app/build',
   },
